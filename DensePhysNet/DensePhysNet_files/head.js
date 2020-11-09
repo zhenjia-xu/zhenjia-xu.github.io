f1eca8942d597a87feabd2fdbf029b284c0ac0e0
zhenjia-xu <xuzhenjia1997@gmail.com>
2020-11-09 20:08:12 +0800

    update

--- DensePhysNet/DensePhysNet_files/head.js
+++ DensePhysNet/DensePhysNet_files/head.js
@@ -23,5 +23,4 @@ function parseQuery(query) {
 document.write('\
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>\
     <meta name=viewport content="width=device-width, initial-scale=1">\
-    <link rel="shortcut icon" href="'+params["prefix"]+'images/favicon.ico">\
 ');
